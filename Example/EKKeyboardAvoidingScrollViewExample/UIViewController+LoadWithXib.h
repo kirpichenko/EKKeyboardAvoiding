2cfbfafa2dc21c14429bdeab7e57f0155f6d38d9
Evgeniy Kirpichenko <kirpichenko@Evgeniys-Mac-mini.local>
2012-12-05 17:34:19 +0200

    Universal controller building starting implementation

---
+++ Example/EKKeyboardAvoidingScrollViewExample/UIViewController+LoadWithXib.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+LoadWithXib.h
+//  EKKeyboardAvoidingScrollViewExample
+//
+//  Created by Evgeniy Kirpichenko on 12/5/12.
+//  Copyright (c) 2012 Evgeniy Kirpichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (LoadWithXib)
+
+@end
---
+++ Example/EKKeyboardAvoidingScrollViewExample/UIViewController+LoadWithXib.m
@@ -0,0 +1,17 @@
+//
+//  UIViewController+LoadWithXib.m
+//  EKKeyboardAvoidingScrollViewExample
+//
+//  Created by Evgeniy Kirpichenko on 12/5/12.
+//  Copyright (c) 2012 Evgeniy Kirpichenko. All rights reserved.
+//
+
+#import "UIViewController+LoadWithXib.h"
+
+#define UIDeviceIsiPad UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
+
+@implementation UIViewController (LoadWithXib)
+
+
+
+@end

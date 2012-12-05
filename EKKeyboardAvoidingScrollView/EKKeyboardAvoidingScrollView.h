7e38b5980b3fb238283e2ff01c0d52d863328766
Evgeniy Kirpichenko <kirpichenko@Evgeniys-Mac-mini.local>
2012-12-05 11:36:45 +0200

    Initial commit

---
+++ EKKeyboardAvoidingScrollView/EKKeyboardAvoidingScrollView.h
@@ -0,0 +1,13 @@
+//
+//  EKKeyboardAvoidingScrollView.h
+//  EKKeyboardAvoidingScrollView
+//
+//  Created by Evgeniy Kirpichenko on 12/5/12.
+//  Copyright (c) 2012 Evgeniy Kirpichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EKKeyboardAvoidingScrollView : UIScrollView
+
+@end
---
+++ EKKeyboardAvoidingScrollView/EKKeyboardAvoidingScrollView.m
@@ -0,0 +1,13 @@
+//
+//  EKKeyboardAvoidingScrollView.m
+//  EKKeyboardAvoidingScrollView
+//
+//  Created by Evgeniy Kirpichenko on 12/5/12.
+//  Copyright (c) 2012 Evgeniy Kirpichenko. All rights reserved.
+//
+
+#import "EKKeyboardAvoidingScrollView.h"
+
+@implementation EKKeyboardAvoidingScrollView
+
+@end

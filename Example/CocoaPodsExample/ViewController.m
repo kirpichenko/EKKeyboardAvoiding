471be18b334b84aeb6286b1f93c42f58843e8e1b
Andrew Romanov <scalli-k-be@ya.ru>
2018-03-20 12:33:08 +0700

    added cocoa pods example

--- Example/CocoaPodsExample/ViewController.m
+++ Example/CocoaPodsExample/ViewController.m
@@ -7,22 +7,54 @@
 //
 
 #import "ViewController.h"
+@import EKKeyboardAvoiding;
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (nonatomic, strong) IBOutlet UIScrollView* scrollView;
+@property (nonatomic, strong) IBOutlet UITextField* textField;
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	[self.scrollView ek_setKeyboardAvoidingEnabled:YES];
 }
 
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+#pragma mark UITextFieldDelegate
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField        // return NO to disallow editing.
+{
+	return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField          // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+{
+	return YES;
+}
+
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField               // called when clear button pressed. return NO to ignore (no notifications)
+{
+	return YES;
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	[self.textField resignFirstResponder];
+	
+	return YES;
+}
+
+
+- (void)dealloc
+{
+	[self.scrollView ek_setKeyboardAvoidingEnabled:NO];
 }
 
 
